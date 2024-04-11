b5e35cf1cf6ba2066a1b509f312b7f7c4ca417ff
Yaroslav Klimas <yarklim@gmail.com>
2024-04-11 10:01:10 +0300

    added swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,9 +1,18 @@
 import Swiper from 'swiper';
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const swiperHero = new Swiper('.swiper', {
   speed: 1500,
   loop: true,
+  autoplay: {
+    delay: 3000,
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
